15caebd69ef914526c788cbc21ed61711836ce90
tsvetoslav.petkov <tsvetoslav.andreev.petkov@gmail.com>
2022-04-04 20:33:44 +0300

    base routing

--- docrate/src/app/app.module.ts
+++ docrate/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
